e84c5af71ee15ee83270583270e9e79f45de52b3
Tomás De Los Santos <delossantostomas12@gmail.com>
2022-05-18 23:21:34 -0300

    Responsive + Media Queries

--- js/app.js
+++ js/app.js
@@ -1,35 +1,46 @@
 const productList = document.getElementById("products");
 const filters = document.getElementsByClassName("filter__btn");
+const minifilters = document.getElementsByClassName("minifilter__btn");
+const filter = document.getElementById("filter");
 
 let product;
 fetch("./js/products.json")
     .then(response => response.json())
     .then(data => {
         product = data;
-        if(productList.classList.contains("highlight")){
+        if (productList.classList.contains("highlight")) {
             createList(data.filter(d => d.highlight == true));
-            
-        } else{
+
+        } else {
             createList(product);
         }
     });
 
 
 document.addEventListener("click", (e) => {
-    if(e.target && e.target.matches("button.card__btn")){
-        console.log("hola");
-    }
+    // if(e.target && e.target.matches("button.card__btn")){
+    //     console.log("hola");
+    // }
 
-    if(e.target && e.target.matches("h4.filter__btn")){
+    if (e.target && (e.target.matches("h4.filter__btn") || e.target.matches("h4.minifilter__btn"))) {
         e.target.classList.contains("all") ? (createList(product), filterStyle(), e.target.classList.add("active")) : null;
         e.target.classList.contains("acc") ? (createList(product.filter(p => p.type == "acc")), filterStyle(), e.target.classList.add("active")) : null;
         e.target.classList.contains("sh") ? (createList(product.filter(p => p.type == "sh")), filterStyle(), e.target.classList.add("active")) : null;
         e.target.classList.contains("jb") ? (createList(product.filter(p => p.type == "jb")), filterStyle(), e.target.classList.add("active")) : null;
         e.target.classList.contains("vela") ? (createList(product.filter(p => p.type != "sh" && p.type != "jb" && p.type != "acc" && p.type != "kit")), filterStyle(), e.target.classList.add("active")) : null;
         e.target.classList.contains("kit") ? (createList(product.filter(p => p.type == "kit")), filterStyle(), e.target.classList.add("active")) : null;
-        
+    }
+    if(e.target && e.target.matches("h4.minifilter__btn")){
+        e.target.classList.add("active");
+        filter.classList.toggle("active");
+    }
+
+    if(e.target && e.target.matches("i.minifilter__menu")){
+        e.target.classList.toggle("active");
+        filter.classList.toggle("active");
     }
 });
+
 
 {/* <div class="card">
     <img class="card__img" src="./assets/acc__mango.jpg" alt="">
@@ -46,6 +57,7 @@ document.addEventListener("click", (e) => {
 const createCard = (product) => {
     const cont = document.createElement("div");
     cont.classList.add("card");
+    cont.classList.add("splide__slide");
     const img = document.createElement("img");
     img.classList.add("card__img");
     img.setAttribute("src", product.img);
@@ -70,14 +82,19 @@ const createCard = (product) => {
 
 const createList = (products) => {
     productList.innerHTML = '';
-    for(let p of products){
+    for (let p of products) {
         createCard(p);
     }
 }
 
 const filterStyle = () => {
-    for(let f of filters){
-        if(f.classList.contains("active")){
+    for (let f of filters) {
+        if (f.classList.contains("active")) {
+            f.classList.remove("active");
+        }
+    }
+    for (let f of minifilters) {
+        if (f.classList.contains("active")) {
             f.classList.remove("active");
         }
     }
